c7b61279a371a4dfe8f7c8191f087ba01369e185
yangqi <13507615840@163.com>
2018-10-30 12:04:58 +0800

    provider 异常处理

--- dubbo-server/src/main/java/com/boot/dubbo/server/service/UserServiceImpl.java
+++ dubbo-server/src/main/java/com/boot/dubbo/server/service/UserServiceImpl.java
@@ -8,31 +8,34 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.boot.dubbo.api.api.IUserService;
 import com.boot.dubbo.api.entity.User;
 import com.boot.dubbo.api.mapper.UserMapper;
+import com.dubbo.common.util.exception.ServiceException;
 
 /**
- * 
- * @Description:    TODO(用一句话描述该文件做什么)   
- * @author: yagnqi
- * @email : yangqi@ywwl.com 
- * @date:   2018年10月10日 下午12:48:40   
  * @version V1.0
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author: yagnqi
+ * @email : yangqi@ywwl.com
+ * @date: 2018年10月10日 下午12:48:40
  */
-@Service(version = "${demo.service.version}")
+@Service(version = "${demo.service.version}", filter = {"exceptionFilter", "providerExceptionFilter"})
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
-	@Override
-	public List<User> findUser(User param) {
+    @Override
+    public List<User> findUser(User param) {
 
-		return this.baseMapper.selectList(new QueryWrapper<>());
-	}
+        return this.baseMapper.selectList(new QueryWrapper<>());
+    }
 
-	@Override
-	public User getUser() {
+    @Override
+    public User getUser() {
 
-		User user = new User();
-		user.setName("name");
-		return user;
-	}
-	
-	
+        User user = new User();
+        user.setName("name");
+        if (user.getId() == null) {
+            throw new ServiceException("自定义异常");
+        }
+        return user;
+    }
+
+
 }
