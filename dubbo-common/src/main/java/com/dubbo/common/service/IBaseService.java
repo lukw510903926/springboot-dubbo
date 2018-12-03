1152366c93e9dbe4b5ee28332de8b4339ce36181
yangqi <13507615840@163.com>
2018-12-03 11:03:16 +0800

    getOne 方法 唯一性校验方法修改

--- dubbo-common/src/main/java/com/dubbo/common/service/IBaseService.java
+++ dubbo-common/src/main/java/com/dubbo/common/service/IBaseService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.toolkit.ExceptionUtils;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.TableInfoHelper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
@@ -17,14 +18,14 @@ import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * @version V1.0
- * @Description: TODO(用一句话描述该文件做什么)
+ * @Description: service 基础类
  * @author: yagnqi
  * @email : yangqi@ywwl.com
  * @date: 2018年10月11日 下午1:30:43
  */
 public interface IBaseService<T> extends IService<T> {
 
-    default boolean check(List<T> list, Serializable uid, String idProperty) {
+    default boolean check(List<T> list, Serializable uid) {
 
         if (CollectionUtils.isEmpty(list)) {
             return true;
@@ -34,6 +35,12 @@ public interface IBaseService<T> extends IService<T> {
             return false;
         }
         T t = list.get(0);
+        String idProperty;
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(t.getClass());
+        if (null == tableInfo || StringUtils.isEmpty(tableInfo.getKeyProperty())) {
+            throw ExceptionUtils.mpe("Error:  Can not execute. Could not find @TableId.");
+        }
+        idProperty = tableInfo.getKeyProperty();
         String oid = ReflectionUtils.getter(t, idProperty).toString();
         return oid.equals(uid);
     }
@@ -42,9 +49,9 @@ public interface IBaseService<T> extends IService<T> {
         return list(new QueryWrapper<>(t));
     }
 
-    default T getOne(T t){
+    default T getOne(T t) {
 
-        return getOne(new QueryWrapper<>(t));
+        return getOne(new QueryWrapper<>(t), true);
     }
 
     default IPage<T> page(Page<T> page, T t) {
