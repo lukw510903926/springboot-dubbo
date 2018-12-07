38994e6ff15a4bb65b7ba58d6118d6efd543683c
yangqi <13507615840@163.com>
2018-12-07 13:52:23 +0800

    统一异常处理

--- boot-mvc/src/main/java/com/boot/dubbo/mvc/controller/GlobalExceptionHandler.java
+++ boot-mvc/src/main/java/com/boot/dubbo/mvc/controller/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.boot.dubbo.mvc.controller;
 
 import java.util.Map;
 
+import com.dubbo.common.util.exception.ParameterException;
+import com.dubbo.common.util.exception.ServiceException;
 import com.dubbo.common.web.RestResult;
 import com.dubbo.common.web.WebUtils;
 import org.slf4j.Logger;
@@ -38,6 +40,26 @@ public class GlobalExceptionHandler {
         Map<String, Object> map = WebUtils.getRequestParam();
         logger.error("统一异常处理 :参数 : {},异常信息 : {}", map, exception);
         return RestResult.fail(map, "操作失败!");
+    }
+
+
+    @ResponseBody
+    @ExceptionHandler(ServiceException.class)
+    public RestResult<Map<String,Object>> serviceException(ServiceException exception){
+
+        Map<String, Object> params = WebUtils.getRequestParam();
+        logger.error("系统异常 : {}",exception);
+        return RestResult.fail(params,exception.getCode(),exception.getMsg());
+    }
+
+
+    @ResponseBody
+    @ExceptionHandler(ParameterException.class)
+    public RestResult<Map<String,Object>> serviceException(ParameterException exception){
+
+        Map<String, Object> params = WebUtils.getRequestParam();
+        logger.error("参数异常 : {}",exception);
+        return RestResult.fail(params,exception.getCode(),exception.getMsg());
     }
 
     /**
