53d4f9edf20855ff3a8b4503a5974b2d907fbc0b
yangqi <13507615840@163.com>
2018-12-07 13:54:26 +0800

    CacheManager 配置

--- boot-mvc/src/main/java/com/boot/dubbo/mvc/config/cache/CacheConfig.java
+++ boot-mvc/src/main/java/com/boot/dubbo/mvc/config/cache/CacheConfig.java
@@ -28,23 +28,23 @@ public class CacheConfig {
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory) {
 
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();  // 生成一个默认配置，通过config对象即可对缓存进行自定义配置
+        // 生成一个默认配置，通过config对象即可对缓存进行自定义配置
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
         // 设置缓存的默认过期时间，也是使用Duration设置  不缓存空值
         config = config.entryTtl(Duration.ofMinutes(1)).disableCachingNullValues();
         // 设置一个初始化的缓存空间set集合
         Set<String> cacheNames = new HashSet<>();
         cacheNames.add("my-redis-cache1");
         cacheNames.add("my-redis-cache2");
-
         // 对每个缓存空间应用不同的配置
         Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
         configMap.put("my-redis-cache1", config);
         configMap.put("my-redis-cache2", config.entryTtl(Duration.ofSeconds(120)));
-
-        RedisCacheManager cacheManager = RedisCacheManager.builder(factory)     // 使用自定义的缓存配置初始化一个cacheManager
-                .initialCacheNames(cacheNames)  // 注意这两句的调用顺序，一定要先调用该方法设置初始化的缓存名，再初始化相关的配置
+        // 使用自定义的缓存配置初始化一个cacheManager
+        // 注意这两句的调用顺序，一定要先调用该方法设置初始化的缓存名，再初始化相关的配置
+        return RedisCacheManager.builder(factory)
+                .initialCacheNames(cacheNames)
                 .withInitialCacheConfigurations(configMap)
                 .build();
-        return cacheManager;
     }
 }
