44b3dde54c63f85ff5ddd144d4e05098baffa628
yangqi <13507615840@163.com>
2018-12-07 11:34:31 +0800

    异常处理

--- dubbo-common/src/main/java/com/dubbo/common/util/exception/ParameterException.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/exception/ParameterException.java
@@ -10,23 +10,52 @@ package com.dubbo.common.util.exception;
 
 public class ParameterException extends RuntimeException {
 
-    public ParameterException() {
-        super();
-    }
+    /**
+     * 状态码
+     */
+    private int code;
+
+    /**
+     * 错误消息
+     */
+    private String msg;
 
     public ParameterException(String message) {
+        this(1000, message);
+    }
+
+    public ParameterException(int code, String message) {
         super(message);
+        this.code = code;
+        this.msg = message;
     }
 
     public ParameterException(String message, Throwable cause) {
         super(message, cause);
+        this.code = 1000;
+        this.msg = message;
     }
 
     public ParameterException(Throwable cause) {
         super(cause);
+        this.code = 1000;
     }
 
-    protected ParameterException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public int getCode() {
+        return code;
+    }
+
+    public ParameterException setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public ParameterException setMsg(String msg) {
+        this.msg = msg;
+        return this;
     }
 }
--- dubbo-common/src/main/java/com/dubbo/common/util/exception/ServiceException.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/exception/ServiceException.java
@@ -1,5 +1,6 @@
 package com.dubbo.common.util.exception;
 
+
 /**
  * <p>
  *
@@ -10,19 +11,56 @@ package com.dubbo.common.util.exception;
  **/
 public class ServiceException extends RuntimeException {
 
+    /**
+     * 状态码
+     */
+    private int code;
+
+    /**
+     * 错误消息
+     */
+    private String msg;
+
     public ServiceException() {
-        super();
+        this(1000, "操作失败");
     }
 
     public ServiceException(String message) {
+        this(1000, message);
+    }
+
+    public ServiceException(int code, String message) {
         super(message);
+        this.code = code;
+        this.msg = message;
     }
 
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
+        this.code = 1000;
+        this.msg = message;
     }
 
     public ServiceException(Throwable cause) {
         super(cause);
+        this.code = 1000;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public ServiceException setCode(int code) {
+        this.code = code;
+        return this;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public ServiceException setMsg(String msg) {
+        this.msg = msg;
+        return this;
     }
 }
