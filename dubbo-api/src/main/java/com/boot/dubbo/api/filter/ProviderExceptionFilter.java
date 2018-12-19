e9d055077d0b2f32a330f560bc514d0e4238fac1
yangqi <13507615840@163.com>
2018-12-19 17:29:55 +0800

    自定义异常处理

--- dubbo-api/src/main/java/com/boot/dubbo/api/filter/ProviderExceptionFilter.java
+++ dubbo-api/src/main/java/com/boot/dubbo/api/filter/ProviderExceptionFilter.java
@@ -23,38 +23,30 @@ public class ProviderExceptionFilter implements Filter {
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+
         Result result = invoker.invoke(invocation);
         if (result.hasException() && GenericService.class != invoker.getInterface()) {
             try {
                 Throwable exception = result.getException();
-
                 if (!(exception instanceof RuntimeException) && (exception instanceof Exception)) {
                     return result;
                 }
-                try {
-                    Method method = invoker.getInterface().getMethod(invocation.getMethodName(), invocation.getParameterTypes());
-                    Class<?>[] exceptionClassses = method.getExceptionTypes();
-                    for (Class<?> exceptionClass : exceptionClassses) {
-                        if (exception.getClass().equals(exceptionClass)) {
-                            return result;
-                        }
+                Method method = invoker.getInterface().getMethod(invocation.getMethodName(), invocation.getParameterTypes());
+                Class<?>[] exceptionClassses = method.getExceptionTypes();
+                for (Class<?> exceptionClass : exceptionClassses) {
+                    if (exception.getClass().equals(exceptionClass)) {
+                        return result;
                     }
-                } catch (NoSuchMethodException e) {
-                    return result;
                 }
-
-                // for the exception not found in method's signature, print ERROR message in server's log.
                 logger.error("dubbo provider has undeclared exception which called by " + RpcContext.getContext().getRemoteHost()
                         + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
                         + ", exception: " + exception.getClass().getName() + ": " + exception.getMessage(), exception);
 
-                // directly throw if exception class and interface class are in the same jar file.
                 String serviceFile = ReflectUtils.getCodeBase(invoker.getInterface());
                 String exceptionFile = ReflectUtils.getCodeBase(exception.getClass());
                 if (serviceFile == null || exceptionFile == null || serviceFile.equals(exceptionFile)) {
                     return result;
                 }
-                // directly throw if it's JDK exception
                 String className = exception.getClass().getName();
                 if (className.startsWith("java.") || className.startsWith("javax.")) {
                     return result;
@@ -63,10 +55,9 @@ public class ProviderExceptionFilter implements Filter {
                 if (exception instanceof RpcException) {
                     return result;
                 }
-
                 // otherwise, wrap with RuntimeException and throw back to the client
                 return new RpcResult(new RuntimeException(StringUtils.toString(exception)));
-            } catch (Throwable e) {
+            } catch (Exception e) {
                 logger.warn("Fail to ExceptionFilter when called by " + RpcContext.getContext().getRemoteHost()
                         + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
                         + ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
