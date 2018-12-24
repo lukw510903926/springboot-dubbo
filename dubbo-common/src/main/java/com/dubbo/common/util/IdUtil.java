f16945572f25439eed500259ca4bf90c79bf1348
yangqi <13507615840@163.com>
2018-12-24 14:16:35 +0800

    id生成工具类

--- dubbo-common/src/main/java/com/dubbo/common/util/IdUtil.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/IdUtil.java
@@ -1,6 +1,12 @@
 package com.dubbo.common.util;
 
+import org.apache.commons.lang3.StringUtils;
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+
+import java.io.Serializable;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * <p>
@@ -12,8 +18,43 @@ import java.util.UUID;
  **/
 public class IdUtil {
 
-    public static String uuid(){
+    private static final ThreadLocal<Integer> SEED = ThreadLocal.withInitial(() -> 0);
 
-        return UUID.randomUUID().toString().replace("-","");
+    private static final String MACHINE_ID;
+
+    static {
+        SystemInfo systemInfo = new SystemInfo();
+        HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
+        MACHINE_ID = hardwareAbstractionLayer.getNetworkIFs()[0].getMacaddr().replace(":", "").toUpperCase();
+    }
+
+    public static String uuid() {
+
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public static String generate(String prefix) {
+
+        if (!StringUtils.isBlank(prefix) && prefix.length() <= 4) {
+            int seed = SEED.get();
+            int random = ThreadLocalRandom.current().nextInt(0, 9999);
+            Serializable[] tempArray = {System.currentTimeMillis(), random, Thread.currentThread().getId(), MACHINE_ID, seed};
+            String tmp = StringUtils.join(tempArray);
+            int over = tmp.length() - (32 - prefix.length());
+            if (over > 0) {
+                tmp = StringUtils.substring(tmp, over);
+            } else {
+                tmp = StringUtils.leftPad(tmp, 32 - prefix.length(), '0');
+            }
+
+            ++seed;
+            if (seed > 9999) {
+                seed = 0;
+            }
+            SEED.set(seed);
+            return prefix + tmp;
+        } else {
+            throw new IllegalArgumentException("prefix illegal");
+        }
     }
 }
