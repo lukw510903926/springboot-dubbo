070e2c3c4d3d095cd96fa2b85277a50f2738a288
yangqi <13507615840@163.com>
2018-12-24 17:30:22 +0800

    java 配置消费者

---
+++ dubbo-server/src/main/java/com/boot/dubbo/server/service/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package com.boot.dubbo.server.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.boot.dubbo.api.api.IProductService;
+import com.boot.dubbo.api.entity.Product;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <p>
+ *
+ * @author yangqi
+ * @Description </p>
+ * @email yangqi@ywwl.com
+ * @since 2018/12/24 17:17
+ **/
+@Service
+public class ProductServiceImpl implements IProductService {
+
+    @Override
+    public List<Product> list(Product product) {
+        return Collections.singletonList(product);
+    }
+}
