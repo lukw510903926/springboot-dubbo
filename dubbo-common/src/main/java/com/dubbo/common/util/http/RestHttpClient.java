c5d3ff8f876dc8844914a55a560c044fd1131329
yangqi <13507615840@163.com>
2018-12-27 17:48:44 +0800

    http 请求添加form表单请求方式

--- dubbo-common/src/main/java/com/dubbo/common/util/http/RestHttpClient.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/http/RestHttpClient.java
@@ -2,12 +2,14 @@ package com.dubbo.common.util.http;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.commons.collections4.MapUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import com.alibaba.fastjson.JSONObject;
 
@@ -22,7 +24,12 @@ public class RestHttpClient {
 
     private static RestTemplate restTemplate = new RestTemplate();
 
-    private RestHttpClient(){}
+    private static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON_UTF8;
+
+    private static final MediaType MULTIPART_FORM_DATA = MediaType.MULTIPART_FORM_DATA;
+
+    private RestHttpClient() {
+    }
 
     static {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
@@ -31,9 +38,9 @@ public class RestHttpClient {
         restTemplate.setRequestFactory(requestFactory);
     }
 
-    public static String get(String url){
+    public static String get(String url) {
 
-        return get(url,String.class);
+        return get(url, String.class);
     }
 
     /**
@@ -56,29 +63,32 @@ public class RestHttpClient {
      */
     public static <V> V get(String url, Class<V> resultType, Map<String, String> header) {
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(header(header));
+        HttpEntity<String> requestEntity = new HttpEntity<>(header(header, APPLICATION_JSON_UTF8));
         return restTemplate.exchange(url, HttpMethod.GET, requestEntity, resultType).getBody();
     }
 
-    public static String post(String url,Object param){
+    public static String post(String url, Object param) {
 
-        return post(url,param,String.class);
+        return post(url, param, String.class, new HashMap<>());
     }
 
     /**
+     * json 请求
+     *
      * @param url        请求参数
      * @param resultType 返回结果类型
      * @param param      请求参数
      * @param <V>
      * @return
      */
-    public static <V> V post(String url, Object param,Class<V> resultType) {
+    public static <V> V post(String url, Object param, Class<V> resultType) {
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(JSONObject.toJSONString(param), header(new HashMap<>()));
-        return restTemplate.postForObject(url, requestEntity, resultType);
+        return post(url, param, resultType, new HashMap<>());
     }
 
     /**
+     * json 请求
+     *
      * @param url        请求地址
      * @param resultType 返回结果类型
      * @param param      请求参数
@@ -86,17 +96,58 @@ public class RestHttpClient {
      * @param <V>
      * @return
      */
-    public static <V> V post(String url, Object param,Class<V> resultType, Map<String, String> header) {
+    public static <V> V post(String url, Object param, Class<V> resultType, Map<String, String> header) {
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(JSONObject.toJSONString(param), header(header));
+        HttpEntity<String> requestEntity = new HttpEntity<>(JSONObject.toJSONString(param), header(header, APPLICATION_JSON_UTF8));
         return restTemplate.postForObject(url, requestEntity, resultType);
     }
 
-    private static HttpHeaders header(Map<String, String> header) {
+    /**
+     * form 表单请求
+     *
+     * @param url
+     * @param param
+     * @return
+     */
+    public static String postForm(String url, MultiValueMap<String, Object> param) {
+
+        return postForm(url, param, String.class, new HashMap<>());
+    }
+
+    /**
+     * form 表单请求
+     *
+     * @param url
+     * @param param
+     * @param header
+     * @return
+     */
+    public static String postForm(String url, MultiValueMap<String, Object> param, Map<String, String> header) {
+
+        return postForm(url, param, String.class, header);
+    }
+
+    /**
+     * form 表单请求
+     *
+     * @param url
+     * @param param
+     * @param resultType
+     * @param header
+     * @param <V>
+     * @return
+     */
+    public static <V> V postForm(String url, MultiValueMap<String, Object> param, Class<V> resultType, Map<String, String> header) {
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(param, header(header, MULTIPART_FORM_DATA));
+        return restTemplate.postForObject(url, requestEntity, resultType);
+    }
+
+    private static HttpHeaders header(Map<String, String> header, MediaType contentType) {
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType("application/json; charset=UTF-8"));
-        headers.add("Accept", MediaType.APPLICATION_JSON_UTF8.toString());
+        headers.setContentType(contentType);
+        headers.add("Accept", contentType.toString());
         if (MapUtils.isNotEmpty(header)) {
             header.forEach(headers::add);
         }
