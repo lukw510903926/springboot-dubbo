a5e21677ca226f02d77821c735a367f2ace3483e
yangqi <13507615840@163.com>
2018-12-28 11:15:12 +0800

    日志

--- dubbo-api/src/main/java/com/boot/dubbo/api/filter/ProviderExceptionFilter.java
+++ dubbo-api/src/main/java/com/boot/dubbo/api/filter/ProviderExceptionFilter.java
@@ -24,6 +24,7 @@ public class ProviderExceptionFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
 
+        logger.info(" ==Provider Exception Filter==");
         Result result = invoker.invoke(invocation);
         if (result.hasException() && GenericService.class != invoker.getInterface()) {
             try {
