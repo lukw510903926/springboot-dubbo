e035f44190fdb47c7aebe812db4484c73fe71311
yangqi <13507615840@163.com>
2019-01-03 15:57:17 +0800

    nacos 配置中心

--- dubbo-nacos/dubbo-nacos-consumer/src/main/java/com/boot/dubbo/nacos/controller/ResourceController.java
+++ dubbo-nacos/dubbo-nacos-consumer/src/main/java/com/boot/dubbo/nacos/controller/ResourceController.java
@@ -3,6 +3,7 @@ package com.boot.dubbo.nacos.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.boot.dubbo.api.api.IResourceService;
 import com.boot.dubbo.api.entity.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
  * @email yangqi@ywwl.com
  * @since 2018/12/29 17:47
  **/
+@Slf4j
 @RestController
 @RequestMapping("resource")
 public class ResourceController {
@@ -25,8 +27,13 @@ public class ResourceController {
     private IResourceService resourceService;
 
     @GetMapping("/list")
-    public List<Resource> list(){
+    public List<Resource> list() {
 
         return this.resourceService.list();
+    }
+
+    @GetMapping("weChat")
+    public String weChat() {
+        return this.resourceService.weChatProperties();
     }
 }
