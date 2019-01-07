afd3d05bf3f7bc619f3a6f418e5ed876c3c25e61
yangqi <13507615840@163.com>
2019-01-07 11:54:56 +0800

    无效注解去除 程序默认从配置中心读取 dataId 为应用名称及spring.application.name的配置文件

--- dubbo-nacos/dubbo-nacos-provider/src/main/java/com/boot/dubbo/nacos/NacosProviderApplication.java
+++ dubbo-nacos/dubbo-nacos-provider/src/main/java/com/boot/dubbo/nacos/NacosProviderApplication.java
@@ -1,7 +1,6 @@
 package com.boot.dubbo.nacos;
 
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2018/12/29 17:28
  **/
 @SpringBootApplication
-@NacosConfigurationProperties(dataId = "dubbo-nacos-provider.properties",autoRefreshed = true)
 @DubboComponentScan(basePackages = "com.boot.dubbo.nacos.service")
 public class NacosProviderApplication {
 
