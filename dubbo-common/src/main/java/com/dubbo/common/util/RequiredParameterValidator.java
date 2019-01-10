085a924db9d029a28694c443e0970cd91f56fa88
yangqi <13507615840@163.com>
2019-01-10 11:14:46 +0800

    支付宝账单下载

--- dubbo-common/src/main/java/com/dubbo/common/util/RequiredParameterValidator.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/RequiredParameterValidator.java
@@ -67,7 +67,7 @@ public class RequiredParameterValidator {
                     Object value = ReflectionUtils.getFieldValue(object, key);
                     if (value == null || StringUtils.isEmpty(value.toString())) {
                         NotNull notNull = fieldMap.get(key).getAnnotation(NotNull.class);
-                        String property = notNull == null ? key : notNull.value();
+                        String property = notNull == null || StringUtils.isBlank(notNull.value()) ? key : notNull.value();
                         stringBuffer.append(property).append(" 不可为空 ! ");
                     }
                 }
