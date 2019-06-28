65f8b06ce653f5f0c51492794564a49017801460
yangqi <2622319737@qq.com>
2019-06-28 13:52:40 +0800

    注释添加

--- dubbo-common/src/main/java/com/dubbo/common/util/alibaba/AliPayUtil.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/alibaba/AliPayUtil.java
@@ -27,41 +27,59 @@ import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-
+/**
+ * @author : yangqi
+ * @createTime : 2019-06-28 13:30
+ * @email : lukewei@mockuai.com
+ * @description :
+ */
 @Data
 public class AliPayUtil {
 
     private static Logger logger = LoggerFactory.getLogger(AliPayUtil.class);
 
-    //https://openapi.alipay.com/gateway.do
-    private String apiUrl;
+    private static final String API_URL = "https://openapi.alipay.com/gateway.do";
 
+    /**
+     * appID
+     */
     private String appId;
 
+    /**
+     * 应用私钥
+     */
     private String privateKey;
 
+    /**
+     * 支付宝公钥
+     */
     private String publicKey;
 
-    private String mchId;
-
     private static AlipayClient alipayClient;
-
 
     public static final String DEFAULT_CHARSET = "UTF-8";
 
     public static final Charset ALI_PAY_BILL_CHARSET = Charset.forName("GBK");
 
     //以下配置写单元测试用的
-    private static String privateKeyStr = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1rHVgFTtOwMYgoZnn/La3X51oB45uJ7UhdeXpkVPy935w1pThL9shN+2NKWkmEUbB0hKbvR6hDQBVDoobxCKQBL5Qouj/XoDYppXvLUPPNEm/fS7pQMcawT7g7IJn/SzfkEDfKYt92zxwfVMEabkfrciOvEv9ijvX99EV2vogvA6pHPGTsxlVmF6qttbfTlebaqULBc9o6NgbJ3tM0absRVeZqLVOJ3jJFrSIb6l5Qzd6UeqaclBlV58yydxHV0k8BY4ufSqivgCCNY/1kI5N7BIe0sCf8PoxtEGqzC6EGmQaW6HOmn8bL3TbllQemGoDRoXBfbH/VfzHNKLwMy2pAgMBAAECggEAKJr7QRZ9wwRjRDfaZnedfy7nx7CorQZC/c+UK7hHwy5+HEpZzFsd5zbQ6ZQv5EpIPlntvtFuXyDAykINrFcSQo5M20kOOsWslr/lEyhS16UgNqSML6ZC2kAHBc7W9tcniHB9j2OBwgeWYq4gOARJVENLaFu9wFV9OrAxE2l2Lop4/NeFEvRPZSnAB91o0pBW3+kSg0cjuMo9NkvJYAF9/ZyjBjCu1JpmDkoUfppEdA579iaKFTzoZt16j0IxOS66UucKYRKW8997FoGSZB8jJcMBNVu9LKLZN8ryIpBfhzeqNVOqlJaRA66VsX5kzBmvQSMP0DxVjR4mPW6LOw/gAQKBgQDuA+xrTcG9PjNyq6XIY9mknUh6st+OGdHD4OTUE0sG6tZauTAnA6nlcUk7EUV4I8/aI7uZxcmiacH2TcAwvE37VmrnmSQXXJ+uREk5GrAwA783KKwUrGjzIpUV5ZrHKj5bUlCk63BqW2zl0WUyv+APKbzugWnaFvqTyHPXTr5GgQKBgQDDZq8F4WRixtdWyUhpOOJJ2RVmY3INTJvwAZ48wn5pNsv0BOXNiNODz8LMyGsnKBl2tBRIy5avUKD1tbKOrrCZyZ4Nw63xL6K+WoDyOp9eEHbVyDSb3bhND1QgfSUQQPyWC+PXaaVMoq+FHJNzHicHeJsNDl72XJ4uxRDlv3ZjKQKBgGQUqEOSZYWScwWjoUj6m1h5fjGBpzjUKHzpwq5eEm0tL8bkGZg+FSFSGk3I/GIBBPQflFGbdiZhu9o6ZTbY5x1o//i0TxW3r7kSI4vsaes2mB+0bW8f891QTEnxyrw2ShQSL7C5AwLlwIt+GcRWI/ucPEI5jm3SvbiIUBVXmfCBAoGBAJqicHAe5uqkoVW1BPEAa1Q1z5GvsVqE/gL5xNggsxlgnBcIqTP2zCzMBkU5dlMOkNg3urXBKAalsDvDBwOTi36o58PMz6LHJ+usg88uRWCTdvfH8JpI5MlXECSF5Qbv5vR3hF+842u6c1zMOIQpoIlxEINMlCozMZuhqLWox2/BAoGBAMqlGfqAr26xPOuLDoCi65ToA0g5U5WGtmDnK3XyZRTufLhWgVR57OnL39SIsGpg6s2qj0P/M+sytOtZs7KGy1od6jhMxYhYNIrbdvecSy6c2GaApYhvNmHcY3zWp8DUR8etvWuw2zPbtFF+fByR/sZWez6FcFA4zCn0ocHmVMVT";
+    /**
+     * 应用私钥
+     */
+    private static final String APP_PRIVATE_KEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1rHVgFTtOwMYgoZnn/La3X51oB45uJ7UhdeXpkVPy935w1pThL9shN+2NKWkmEUbB0hKbvR6hDQBVDoobxCKQBL5Qouj/XoDYppXvLUPPNEm/fS7pQMcawT7g7IJn/SzfkEDfKYt92zxwfVMEabkfrciOvEv9ijvX99EV2vogvA6pHPGTsxlVmF6qttbfTlebaqULBc9o6NgbJ3tM0absRVeZqLVOJ3jJFrSIb6l5Qzd6UeqaclBlV58yydxHV0k8BY4ufSqivgCCNY/1kI5N7BIe0sCf8PoxtEGqzC6EGmQaW6HOmn8bL3TbllQemGoDRoXBfbH/VfzHNKLwMy2pAgMBAAECggEAKJr7QRZ9wwRjRDfaZnedfy7nx7CorQZC/c+UK7hHwy5+HEpZzFsd5zbQ6ZQv5EpIPlntvtFuXyDAykINrFcSQo5M20kOOsWslr/lEyhS16UgNqSML6ZC2kAHBc7W9tcniHB9j2OBwgeWYq4gOARJVENLaFu9wFV9OrAxE2l2Lop4/NeFEvRPZSnAB91o0pBW3+kSg0cjuMo9NkvJYAF9/ZyjBjCu1JpmDkoUfppEdA579iaKFTzoZt16j0IxOS66UucKYRKW8997FoGSZB8jJcMBNVu9LKLZN8ryIpBfhzeqNVOqlJaRA66VsX5kzBmvQSMP0DxVjR4mPW6LOw/gAQKBgQDuA+xrTcG9PjNyq6XIY9mknUh6st+OGdHD4OTUE0sG6tZauTAnA6nlcUk7EUV4I8/aI7uZxcmiacH2TcAwvE37VmrnmSQXXJ+uREk5GrAwA783KKwUrGjzIpUV5ZrHKj5bUlCk63BqW2zl0WUyv+APKbzugWnaFvqTyHPXTr5GgQKBgQDDZq8F4WRixtdWyUhpOOJJ2RVmY3INTJvwAZ48wn5pNsv0BOXNiNODz8LMyGsnKBl2tBRIy5avUKD1tbKOrrCZyZ4Nw63xL6K+WoDyOp9eEHbVyDSb3bhND1QgfSUQQPyWC+PXaaVMoq+FHJNzHicHeJsNDl72XJ4uxRDlv3ZjKQKBgGQUqEOSZYWScwWjoUj6m1h5fjGBpzjUKHzpwq5eEm0tL8bkGZg+FSFSGk3I/GIBBPQflFGbdiZhu9o6ZTbY5x1o//i0TxW3r7kSI4vsaes2mB+0bW8f891QTEnxyrw2ShQSL7C5AwLlwIt+GcRWI/ucPEI5jm3SvbiIUBVXmfCBAoGBAJqicHAe5uqkoVW1BPEAa1Q1z5GvsVqE/gL5xNggsxlgnBcIqTP2zCzMBkU5dlMOkNg3urXBKAalsDvDBwOTi36o58PMz6LHJ+usg88uRWCTdvfH8JpI5MlXECSF5Qbv5vR3hF+842u6c1zMOIQpoIlxEINMlCozMZuhqLWox2/BAoGBAMqlGfqAr26xPOuLDoCi65ToA0g5U5WGtmDnK3XyZRTufLhWgVR57OnL39SIsGpg6s2qj0P/M+sytOtZs7KGy1od6jhMxYhYNIrbdvecSy6c2GaApYhvNmHcY3zWp8DUR8etvWuw2zPbtFF+fByR/sZWez6FcFA4zCn0ocHmVMVT";
 
-    private static String publicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtax1YBU7TsDGIKGZ5/y2t1+daAeObie1IXXl6ZFT8vd+cNaU4S/bITftjSlpJhFGwdISm70eoQ0AVQ6KG8QikAS+UKLo/16A2KaV7y1DzzRJv30u6UDHGsE+4OyCZ/0s35BA3ymLfds8cH1TBGm5H63IjrxL/Yo71/fRFdr6ILwOqRzxk7MZVZheqrbW305Xm2qlCwXPaOjYGyd7TNGm7EVXmai1Tid4yRa0iG+peUM3elHqmnJQZVefMsncR1dJPAWOLn0qor4AgjWP9ZCOTewSHtLAn/D6MbRBqswuhBpkGluhzpp/Gy9025ZUHphqA0aFwX2x/1X8xzSi8DMtqQIDAQAB";
+    /**
+     * 应用公钥
+     */
+    private static final String APP_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtax1YBU7TsDGIKGZ5/y2t1+daAeObie1IXXl6ZFT8vd+cNaU4S/bITftjSlpJhFGwdISm70eoQ0AVQ6KG8QikAS+UKLo/16A2KaV7y1DzzRJv30u6UDHGsE+4OyCZ/0s35BA3ymLfds8cH1TBGm5H63IjrxL/Yo71/fRFdr6ILwOqRzxk7MZVZheqrbW305Xm2qlCwXPaOjYGyd7TNGm7EVXmai1Tid4yRa0iG+peUM3elHqmnJQZVefMsncR1dJPAWOLn0qor4AgjWP9ZCOTewSHtLAn/D6MbRBqswuhBpkGluhzpp/Gy9025ZUHphqA0aFwX2x/1X8xzSi8DMtqQIDAQAB";
 
-    //支付宝公钥
-    private static String aliPayPublicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArffz9HAyoCHVGXzqB8iMuEM8zfLn/HCIDmMReiYz0rtUZl981NcLz2SxSiAXWA3gqA0v+HqquViFjRXbxlhuP7JW3lu/VAEUxoby1AAEFXypjT7F++XHD4ddVihllxpPlaCkOzwiAdA/iNYPPIG658XZtnb67vDiHuXUqdOuEyQelIB9u5lfAnhxfXcGrOKZwRchnyxXxwcKRjLwJ7wgXgRxJf8m9Q8qCyLqipukAzlz86kAuARahcf3tIxHjO/9HNRHqGiG1zkZfUYyPKF9vEKevUiNPM00FBohz5HJ9sGRYRFr988VfBThVcAxIcLZz7hiOAcVQy6YM8U84ck1mQIDAQAB";
+    /**
+     * 支付宝公钥
+     */
+    private static final String ALI_PAY_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArffz9HAyoCHVGXzqB8iMuEM8zfLn/HCIDmMReiYz0rtUZl981NcLz2SxSiAXWA3gqA0v+HqquViFjRXbxlhuP7JW3lu/VAEUxoby1AAEFXypjT7F++XHD4ddVihllxpPlaCkOzwiAdA/iNYPPIG658XZtnb67vDiHuXUqdOuEyQelIB9u5lfAnhxfXcGrOKZwRchnyxXxwcKRjLwJ7wgXgRxJf8m9Q8qCyLqipukAzlz86kAuARahcf3tIxHjO/9HNRHqGiG1zkZfUYyPKF9vEKevUiNPM00FBohz5HJ9sGRYRFr988VfBThVcAxIcLZz7hiOAcVQy6YM8U84ck1mQIDAQAB";
 
     @PostConstruct
     public void initClient() {
-        alipayClient = new DefaultAlipayClient(apiUrl, appId, privateKey, "json", DEFAULT_CHARSET, publicKey, "RSA2");
+        alipayClient = new DefaultAlipayClient(API_URL, appId, privateKey, "json", DEFAULT_CHARSET, publicKey, "RSA2");
     }
 
     /**
@@ -164,9 +182,9 @@ public class AliPayUtil {
     public static void main(String[] args) throws Exception {
 
         String src = "测试测试";
-        String signResult = AlipaySignature.rsa256Sign(src, privateKeyStr, DEFAULT_CHARSET);
-        System.out.println(AlipaySignature.rsa256CheckContent(src, signResult, publicKeyStr, DEFAULT_CHARSET));
-        AlipayClient testClient = new DefaultAlipayClient("https://openapi.alipay.com/gateway.do", "2018111262101925", privateKeyStr, "json", DEFAULT_CHARSET, aliPayPublicKeyStr, "RSA2");
+        String signResult = AlipaySignature.rsa256Sign(src, APP_PRIVATE_KEY, DEFAULT_CHARSET);
+        logger.info("result : {}",AlipaySignature.rsa256CheckContent(src, signResult, APP_PUBLIC_KEY, DEFAULT_CHARSET));
+        AlipayClient testClient = new DefaultAlipayClient(API_URL, "2018111262101925", APP_PRIVATE_KEY, "json", DEFAULT_CHARSET, ALI_PAY_PUBLIC_KEY, "RSA2");
         String orderId = "ee0a227881ed456081b247d42c6d7491";
         AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
         Map<String, Object> content = new HashMap<>();
@@ -175,7 +193,7 @@ public class AliPayUtil {
         AlipayTradeQueryResponse response = testClient.execute(request);
         if (response.isSuccess()) {
             logger.info("AliPayUtil queryTradeWithOrderId invoke success");
-            System.out.println(response.getBuyerUserId());
+            logger.info(response.getBuyerUserId());
         } else {
             logger.error("AliPayUtil queryTradeWithOrderId invoke error");
         }
