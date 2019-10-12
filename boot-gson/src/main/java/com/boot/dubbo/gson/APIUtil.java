de1735e617d920fcf7ba5e8cec3b50a86eb4c516
lkw510903926 <13507615840@163.com>
2019-10-12 23:32:49 +0800

    '测试代码生成';

--- boot-gson/src/main/java/com/boot/dubbo/gson/APIUtil.java
+++ boot-gson/src/main/java/com/boot/dubbo/gson/APIUtil.java
@@ -9,11 +9,14 @@ import java.lang.reflect.Parameter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class APIUtil {
+
+    private static List<String> array = Arrays.asList("wait", "equals", "toString", "hashCode", "getClass", "notify", "notifyAll");
 
     public static void main(String[] args) throws Exception {
 
@@ -26,29 +29,34 @@ public class APIUtil {
     private static void buildCode(Class<?> clazz) {
 
         Method[] methods = clazz.getMethods();
+        StringBuilder builder = new StringBuilder("\nimport org.junit.Test;");
+        builder.append("\nimport java.util.Map; ");
+        builder.append("\nimport java.util.HashMap; \n\n");
+        builder.append("public class ").append(clazz.getSimpleName()).append("Test {");
         for (Method method : methods) {
-            StringBuilder builder = new StringBuilder("\n@Test");
-            builder.append("\n");
             GetMapping annotation = method.getAnnotation(GetMapping.class);
-            if (annotation != null) {
-                String[] url = annotation.value();
-                Parameter[] parameters = method.getParameters();
-                builder.append("public void ").append(method.getName()).append("(){ \n \n");
-                builder.append("    Map<String,Object> param = new HashMap<>(); \n");
-                for (Parameter parameter : parameters) {
-                    builder.append("    param.put(\"").append(parameter.getName()).append("\",\"");
-                    if (Number.class.isAssignableFrom(parameter.getType())) {
-                        builder.append(ThreadLocalRandom.current().nextInt(100, 20000)).append("\");\n");
-                    } else if (Date.class.isAssignableFrom(parameter.getType())) {
-                        builder.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\");\n");
-                    } else {
-                        builder.append(parameter.getName()).append("\");\n");
-                    }
-                }
-                builder.append("}");
-                System.out.println(builder);
+            if (annotation == null) {
+                continue;
             }
+            builder.append("\n\n    @Test\n");
+            Parameter[] parameters = method.getParameters();
+            builder.append("    public void ").append(method.getName()).append("(){ \n \n");
+            builder.append("        String apiPath = \"").append(annotation.value()[0]).append("\";\n");
+            builder.append("        Map<String,Object> param = new HashMap<>(); \n");
+            for (Parameter parameter : parameters) {
+                builder.append("        param.put(\"").append(parameter.getName()).append("\",\"");
+                if (Number.class.isAssignableFrom(parameter.getType())) {
+                    builder.append(ThreadLocalRandom.current().nextInt(100, 20000)).append("\");\n");
+                } else if (Date.class.isAssignableFrom(parameter.getType())) {
+                    builder.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append("\");\n");
+                } else {
+                    builder.append(parameter.getName()).append("\");\n");
+                }
+            }
+            builder.append("    }");
         }
+        builder.append("\n}");
+        System.out.println(builder);
     }
 
     public static void getInstance(String path, List<Class<?>> list) throws Exception {
