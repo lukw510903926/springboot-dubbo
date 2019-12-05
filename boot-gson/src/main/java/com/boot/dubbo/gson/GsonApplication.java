9fcab40dbb69df4159d4e5661181d05c11d443d3
yangqi <2622319737@qq.com>
2019-12-05 15:13:33 +0800

    springboot 版本升级

--- boot-gson/src/main/java/com/boot/dubbo/gson/GsonApplication.java
+++ boot-gson/src/main/java/com/boot/dubbo/gson/GsonApplication.java
@@ -1,28 +1,12 @@
 package com.boot.dubbo.gson;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
 
 @SpringBootApplication
 public class GsonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GsonApplication.class, args);
-    }
-
-    @Bean
-    public HttpMessageConverters messageConverters() {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
-        gsonBuilder.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        Gson gson = gsonBuilder.create();
-        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter(gson);
-        return new HttpMessageConverters(gsonHttpMessageConverter);
     }
 }
