46f4524c75a556c8d641f8734b46edb8102b91a8
yangqi <lkw510903926@163.com>
2020-01-12 11:54:42 +0800

    dubbo 版本升级到2.7.5 mysql 驱动版本升级到8.0 配置文件中的接口版本需要和注解上使用的版本保持一致

--- dubbo-server/src/main/java/com/boot/dubbo/server/DubboServerApplication.java
+++ dubbo-server/src/main/java/com/boot/dubbo/server/DubboServerApplication.java
@@ -1,6 +1,6 @@
 package com.boot.dubbo.server;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,23 +8,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
+ * @author : lukewei
  * @project : dubbo-server
  * @createTime : 2018年7月3日 : 上午9:33:27
- * @author : lukewei
  * @description :
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.boot.dubbo.api.mapper")
-@DubboComponentScan(basePackages = "com.boot.dubbo.server.service")
+@EnableDubbo(scanBasePackages = "com.boot.dubbo.server.service")
 public class DubboServerApplication {
 
-	private static Logger logger = LoggerFactory.getLogger(DubboServerApplication.class);
+    private static Logger logger = LoggerFactory.getLogger(DubboServerApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(DubboServerApplication.class, args);
-		logger.info("dubbo server application start successfully -------");
-	}
+        SpringApplication.run(DubboServerApplication.class, args);
+        logger.info("dubbo server application start successfully -------");
+    }
 
 }
