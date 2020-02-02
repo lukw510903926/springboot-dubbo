0fa6d4eb51d074f344d148c32bccccdef6b5d0f3
yangqi <2622319737@qq.com>
2020-02-02 14:43:34 +0800

    gateway 请求转发

--- dubbo-common/src/main/java/com/dubbo/common/util/http/WebClientUtil.java
+++ dubbo-common/src/main/java/com/dubbo/common/util/http/WebClientUtil.java
@@ -1,19 +1,16 @@
 package com.dubbo.common.util.http;
 
-import com.alibaba.fastjson.JSONObject;
 import com.dubbo.common.util.log.LogUtils;
 import java.io.InputStream;
 import java.time.Duration;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -23,7 +20,6 @@ import reactor.core.publisher.Mono;
 
 /**
  * @author : lukew
- * @project : IDEA
  * @createTime : 2018/4/13 23:29
  * @email : 13507615840@163.com
  * @gitHub : https://github.com/lukw510903926
@@ -63,30 +59,57 @@ public class WebClientUtil {
         return post(uri, parameter, MEDIA_TYPE, resultType);
     }
 
+    /**
+     * @param uri
+     * @param parameter
+     * @param mediaType
+     * @param resultType
+     * @param <V>
+     * @return
+     */
     private static <V> V post(RequestBodySpec uri, Object parameter, MediaType mediaType, Class<V> resultType) {
 
         return uri.contentType(mediaType).body(Mono.justOrEmpty(parameter), Object.class).retrieve()
                 .bodyToMono(resultType).blockOptional(TIME_OUT).orElse(null);
     }
 
+    /**
+     * @param parameter
+     * @param url
+     * @param inputStream
+     * @param fileName
+     * @param resultType
+     * @param <V>
+     * @return
+     */
     public static <V> V postMultipart(Map<String, Object> parameter, String url, InputStream inputStream, String fileName, Class<V> resultType) {
 
         MultipartBodyBuilder builder = new MultipartBodyBuilder();
-        builder.part("fieldPart", "fieldValue");
         builder.part(fileName, inputStream);
-        builder.part("jsonPart", new JSONObject());
         if (MapUtils.isNotEmpty(parameter)) {
             parameter.forEach(builder::part);
         }
-        MultiValueMap<String, HttpEntity<?>> parts = builder.build();
         return post(uri(url, HttpMethod.POST), builder.build(), MULTIPART_FORM, resultType);
     }
 
+    /**
+     * @param url
+     * @param resultType
+     * @param <V>
+     * @return
+     */
     public static <V> V get(String url, Class<V> resultType) {
 
         return uri(url, HttpMethod.GET).retrieve().bodyToMono(resultType).blockOptional(TIME_OUT).orElse(null);
     }
 
+    /**
+     * @param url
+     * @param header
+     * @param resultType
+     * @param <V>
+     * @return
+     */
     public static <V> V get(String url, Map<String, String> header, Class<V> resultType) {
 
         RequestBodySpec uri = uri(url, HttpMethod.GET);
@@ -94,11 +117,21 @@ public class WebClientUtil {
         return uri.retrieve().bodyToMono(resultType).blockOptional(TIME_OUT).orElse(null);
     }
 
+    /**
+     * @param url
+     * @param method
+     * @return
+     */
     private static RequestBodySpec uri(String url, HttpMethod method) {
 
         return WebClient.create().method(method).uri(url);
     }
 
+    /**
+     * @param exchange
+     * @param url
+     * @return
+     */
     public static Mono<Void> exchangeRequest(ServerWebExchange exchange, String url) {
 
         HttpMethod method = HttpMethod.valueOf(exchange.getRequest().getMethodValue());
