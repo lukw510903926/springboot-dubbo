f8e7843b18812563043c8c4b411efd125ab32096
lukewei <lw19901008>
2020-06-07 21:30:06 +0800

    http 请求注册

--- boot-mvc/src/main/java/com/boot/dubbo/mvc/controller/UserController.java
+++ boot-mvc/src/main/java/com/boot/dubbo/mvc/controller/UserController.java
@@ -1,23 +1,28 @@
 package com.boot.dubbo.mvc.controller;
 
-import java.util.Date;
-import java.util.List;
-
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.boot.dubbo.api.entity.Product;
+import com.boot.dubbo.api.entity.User;
 import com.boot.dubbo.mvc.service.IUserService;
 import com.dubbo.common.util.SpringApplicationContext;
+import com.dubbo.common.util.annotation.HttpApi;
 import com.dubbo.common.web.RestResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.boot.dubbo.api.entity.User;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -25,9 +30,8 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @RequestMapping("/list")
+    @HttpApi(url = "/user/list", method = "get", upload = true)
     public RestResult<List<User>> sayHello() {
 
         return RestResult.success(this.userService.list(new QueryWrapper<>()));
@@ -89,14 +93,14 @@ public class UserController {
 
     @PostMapping("/page/list")
     public RestResult<IPage<User>> query(@RequestBody User user) {
-        logger.info("user: {}", user);
+        log.info("user: {}", user);
         Page<User> page = new Page<>(1, 10);
         page.setDesc("test_id");
         return RestResult.success(this.userService.page(page, user));
     }
 
     @GetMapping("/product")
-    public String product(){
-        return JSONObject.toJSONString(SpringApplicationContext.getBean(Product.class));
+    public String product() {
+        return JSON.toJSONString(SpringApplicationContext.getBean(Product.class));
     }
 }
